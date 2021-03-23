bc8031891207596e6cd42aee88e2a5a6f065ce85
Sébastien Verbois <sebastien.verbois@imio.be>
2021-03-23 20:24:08 +0100

    Add CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import contacts
 
 api = FastAPI()
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api.include_router(contacts)
